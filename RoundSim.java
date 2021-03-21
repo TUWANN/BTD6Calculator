14f48ea2c8f6788bf908b8be416fa9f82eb868f3
TUWANN <tuwanathan@gmail.com>
2021-03-20 20:54:59 -0400

    roundsim class outline

---
+++ RoundSim.java
@@ -0,0 +1,17 @@
+public class RoundSim
+{
+    //im thinkin layouts are gonna be its own custom object
+    public boolean Round(int roundNum,)
+    {
+        //simulates a round and returns whether or not it will pass
+        
+    }
+    public float GetTotalDmg()
+    {
+        //calculates all the damage that will be done this round, returns it
+    }
+    public float getTotalBloonHealth(int roundNum)
+    {
+        //calculates the total balloon health of a round, should be ez
+    }
+}
