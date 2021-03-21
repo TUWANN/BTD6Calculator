cbd0c0894311b17f757c047fbe6f984279e48cb4
TUWANN <tuwanathan@gmail.com>
2021-03-20 21:58:07 -0400

    this is harder than i thot

--- RoundSim.java
+++ RoundSim.java
@@ -1,17 +1,22 @@
 public class RoundSim
 {
     //im thinkin layouts are gonna be its own custom object
-    public boolean Round(int roundNum,)
+    public boolean Round(int roundNum) //layout will also be a param
     {
         //simulates a round and returns whether or not it will pass
-        
+        return getTotalVanillaBloonHealth(roundNum) =< getTotalDmg(); //THIS DOES NOT ACCOUNT FOR DIFFERENT IMMUNITIES (CAMO, ETC)
     }
-    public float GetTotalDmg()
+    public float getTotalDmg() //layout will be a param
     {
         //calculates all the damage that will be done this round, returns it
     }
-    public float getTotalBloonHealth(int roundNum)
+    public float getTotalVanillaBloonHealth(int roundNum)
     {
         //calculates the total balloon health of a round, should be ez
+    }
+    //diff balloon types: normal, camo, black, white, purple, lead, CL, CP, CPL?
+    public float getTotalCamoBloonHealth(int roundNum)
+    {
+
     }
 }
